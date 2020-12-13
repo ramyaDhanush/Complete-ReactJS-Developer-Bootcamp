ce9e6d67953ab6dc31192b0471079c6624704003
Ramya D <ramyadhanush99@gmail.com>
2020-12-13 17:34:22 +0530

    Fix : Folder Naming
    Signed-off-by: Ramya D <ramyadhanush99@gmail.com>

--- 7. Context-api-reducers-actions/src/App.js
+++ 7. Context-api-reducers-actions/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import React, { useReducer } from 'react'
+import{ Container }from "reactstrap"
+import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css';
+import { TodoContext } from "./context/TodoContext";
+import todoReducer from './context/reducer'
+import TodoForm from './components/TodoForm';
+import Todos from './components/Todos';
 
 function App() {
+  const [todos, dispatch] = useReducer(todoReducer, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TodoContext.Provider value={{todos,dispatch}}>
+      <Container fluid>
+        <h1>Todo App With Context API</h1>
+        <Todos/>
+        <TodoForm/>
+      </Container>
+
+    </TodoContext.Provider>
   );
 }
 
