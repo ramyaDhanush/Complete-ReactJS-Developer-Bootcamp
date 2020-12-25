d3f28e57c34e7d52634af9f4a367060bad28150a
RamyaDhanush <ramyadhanush99@gmail.com>
2020-12-25 21:36:55 +0530

    Add : Firebase Config Details of Application
    Signed-off-by: RamyaDhanush <ramyadhanush99@gmail.com>

--- 12.Firebase Database/src/utils/config.js
+++ 12.Firebase Database/src/utils/config.js
@@ -1,4 +1,14 @@
 //TODO: add firebase configuration and export it
+export const firebaseConfig = {
+    apiKey: "AIzaSyA56VsRur0jY6bMsEek9oVwVcaQrLnZnTc",
+    authDomain: "my-gitapp-de233.firebaseapp.com",
+    databaseURL: "https://my-gitapp-de233-default-rtdb.firebaseio.com",
+    projectId: "my-gitapp-de233",
+    storageBucket: "my-gitapp-de233.appspot.com",
+    messagingSenderId: "147007397000",
+    appId: "1:147007397000:web:304d2574fb9761531d533d",
+    measurementId: "G-D5W0RT74PM"
+  };
 
 //image configuration
 export const imageConfig = {
