be780f4154af768bd43964b509c6d0af864b7d54
Ramya D <ramyadhanush99@gmail.com>
2020-12-27 00:47:01 +0530

    Complete : Redux store
    Signed-off-by: Ramya D <ramyadhanush99@gmail.com>

--- redux-todo/src/App.js
+++ redux-todo/src/App.js
@@ -2,18 +2,18 @@ import React from "react";
 import {
   Container,
   } from "reactstrap";
-import ".bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import TodoForm from "./components/TodoForm";
-import Todo from "./components/Todo";
+import Todo from "./components/Todos";
 
 import { Provider } from "react-redux";
-
+import store from "./store";
 import "./App.css";
 
 function App() {
   return (
-    <Provider>
-      <Container>
+    <Provider store={store}>
+      <Container fluid>
         <Todo/>
         <TodoForm/>
       </Container>
--- redux-todo/src/components/TodoForm.js
+++ redux-todo/src/components/TodoForm.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 
 import {
-  Container,
   FormGroup,
   Input,
   Button,
